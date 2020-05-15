f5a234ee5c7ba105139d43d5e0b99ec4ef32184d
TrutnevIvan <www.vanya22@gmail.com>
2020-05-15 17:11:46 +0300

    Added 2nd input number

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int x1, x2;
+    std::cout << "Input 1st number: ";
+    std::cin >> x1;
+    std::cout << "Input 2nd number: ";
+    std::cin >> x2;
+
+    return 0;
+}
