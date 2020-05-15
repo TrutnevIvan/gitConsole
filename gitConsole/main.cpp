8b6befe745e79f056fab4ccf2aa1b5df961fabc8
TrutnevIvan <www.vanya22@gmail.com>
2020-05-15 17:58:01 +0300

    added sub

---
+++ gitConsole/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    int x1, x2;
+    std::cout << "Input 1st number: ";
+    std::cin >> x1;
+    std::cout << "Input 2nd number: ";
+    std::cin >> x2;
+    std::cout << "The sum = " << x1+x2 << std::endl;
+    std::cout << "The sub = " << x1-x2 << std::endl;
+
+    return 0;
+}
